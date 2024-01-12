604fd24a9a81f98828ce3d1e53bd96843aeb016f
Orel Vilchise <orel.vilchis@morsa.com.mx>
2024-01-11 21:45:43 -0600

    110123

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 		//Open street  Map
-		var coord = [40.738270, -74.008911]; // <--- coordinates here
+		var coord = [20.711487371482157, -103.32328478114268]; // <--- coordinates here
 
 		var map = L.map('map-canvas', { scrollWheelZoom:false}).setView(coord, 19);
 
