0ec8a18d87989552f41d574c121b83d59e9fb058
Orel Vilchise <orel.vilchis@morsa.com.mx>
2024-03-04 04:05:28 -0600

    invitacion digital_30

--- datos.php
+++ datos.php
@@ -10,11 +10,11 @@
     <div style="width: 100%; height: 180vh; padding: 20px; text-align: center; background-image: url(img/fondo_regalo2.png); background-repeat: no-repeat; background-size: cover;">
         <img src="img/logo/logo_boda_b.png" style="width: 80%;" alt="">
         <div style="width: 100%; margin: 10px; color: #fff;">
-            <p>Damos muchas gracias a Dios por estar viviendo esta experiencia tan hermosa, desde el momento en que decidimos comprometernos solo hemos tenido algo en mente, dedicar nuestras vidas al servicios del Señor.</p>
+            <p>Damos muchas gracias a Dios por estar viviendo esta experiencia tan hermosa, desde el momento en que decidimos comprometernos solo hemos tenido algo en mente, dedicar nuestras vidas al servicios de nuestro Dios.</p>
             <p>Este llamado ha sido confirmado en nuestras vidas desde antes de decidir unir nuestras vidas y ahora que estamos a poco tiempo rogamos a nuestro Dios nos guie conforme a su voluntad agradable y perfecta.</p>
-            <p>Si estas aquí con el deseo de obsequiarnos algo para el dia de nuestra boda, antes que nada damos gracias a Dios por ti vida porque eres parte de la respuesta de Dios a nuestras oraciones.</p>
-            <p>Si Dios permite, unos dias despues de la boda nos mudaremos a Dallas Texas en los Estados Unidos para que con la dirección de Dios podamos seguir preparandonos para el servicio del Señor, es por ello que si tu deseo era darnos un regalo, te quisieramos pedir algo especial, ya que no podremos llevarnos muchas cosas nos encantaria que pudieras depositarnos la cantidad equivalente al regalo que el Señor haya puesto en tu corazon. Este recurso será utilizado para nuestro sustento asi como para nuestros estudios.</p>
-            <p>Si no fuera posible que nos pudieras ayudar de esta forma, con toda tranqulidad te decimos que te entendemos y te pedimos que no te preocupes, si tu deseo es el apoyarnos de esta forma a continuacion te dejamos nuestros datos bancarios.</p>
+            <p>Si estas aquí con el deseo de obsequiarnos algo para el día de nuestra boda, antes que nada damos gracias a Dios por ti vida porque eres parte de la respuesta de Dios a nuestras oraciones.</p>
+            <p>Si Dios permite, unos dias despues de la boda, nos mudaremos a Dallas Texas, Estados Unidos, para que con la dirección de Dios podamos seguir preparandonos para el servicio del Señor, es por ello que si tu deseo era darnos un regalo, te quisieramos pedir algo especial, ya que no podremos llevarnos muchas cosas nos encantaria que pudieras enviarnos la cantidad equivalente al regalo que el Señor haya puesto en tu corazon. Este recurso será utilizado para nuestro sustento así como para nuestros estudios.</p>
+            <p>Si no fuera posible que nos pudieras ayudar de esta forma, con toda tranqulidad te decimos que te entendemos y te pedimos que no te preocupes, pero si tu deseo es el apoyarnos de esta forma a continuación te dejamos nuestros datos para que puedas enviarnos lo que el Señor ponga en tu corazón.</p>
             <p>Cualquier duda que tengas por favor no dudes en contactarnos.</p>
             <hr>
             <p>Banco: BBVA</p>
@@ -22,9 +22,10 @@
             <p>Cuenta CLABE: 012320011503985787</p>
             <p>Titular: Orel Vilchis Martinez</p>
             <hr>
-            <p>Muchas gracias mi hermano y amigo por esta bendición pero lo mas importante porfavor te pedimos que nos tengas en tus oraciones, para que el Señor nos use conforme a su voluntad y para su gloria.</p>
-            <p>Que Dios te bendiga!!!</p>
-            <p><a onclick="abrir_website();" id="a_back" href=""><p><button style="padding: 10px 50px; border-radius: 10px; background-color: #9B8550; color: #fff; border: none;"><b>Regresar</b></button></p></a></p>
+            <p>Muchas gracias mi hermano y amigo por esta bendición, pero lo mas importante porfavor te pedimos que nos tengas en tus oraciones, para que el Señor nos use conforme a su voluntad y para su gloria.</p>
+            <p>Que Dios te bendiga grandemente!!!</p>
+            <hr><br>
+            <a onclick="abrir_website();" id="a_back" href="" style="padding: 10px 50px; border-radius: 10px; background-color: #9B8550; color: #fff; border: none;">Regresar a pagina principal</a>
 
         </div>
     </div>
